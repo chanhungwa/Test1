c51d69a4c1d3c71212eeb922a349a42a2049d38e
M1043804 <chanhungwa@gmail.com>
2018-05-03 16:05:31 -0400

    first commit

---
+++ src/Test1Main.java
@@ -0,0 +1,15 @@
+
+public class Test1Main {
+
+	public static void main(String[] args) {
+		System.out.println("This is test 1");
+		System.out.println("Test function 'addNumb(10,20)' = "+addNum(10,20));
+		for(int i = 0; i<10 ;i++)
+			System.out.println("loop number: "+ (i+1));
+	}
+	
+	public static int addNum(int a, int b) {
+		return a+b;
+	}
+
+}
---
+++ src/Test1TestCase.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class Test1TestCase {
+
+	@Test
+	void test() {
+		assertEquals(Test1Main.addNum(10, 10),20);
+	}
+
+}
