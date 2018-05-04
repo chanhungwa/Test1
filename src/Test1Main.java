f6653d05b2fa5fd59f34166834bd9e0b7b168ce2
M1043804 <chanhungwa@gmail.com>
2018-05-04 10:28:10 -0400

    print hello world

--- src/Test1Main.java
+++ src/Test1Main.java
@@ -6,6 +6,8 @@ public class Test1Main {
 		System.out.println("Test function 'addNumb(10,20)' = "+addNum(10,20));
 		for(int i = 0; i<10 ;i++)
 			System.out.println("loop number: "+ (i+1));
+		
+		System.out.println("Hello World");
 	}
 	
 	public static int addNum(int a, int b) {
