218818f98f46371e07ee08ab30e7e1c66d947384
Patat <chanhungwa@gmail.com>
2018-05-04 20:25:40 -0400

    test line added to Test1Main

--- src/Test1Main.java
+++ src/Test1Main.java
@@ -8,6 +8,7 @@ public class Test1Main {
 			System.out.println("loop number: "+ (i+1));
 		
 		System.out.println(print("Hello World"));
+		System.out.println("new line added");
 	}
 	
 	public static int addNum(int a, int b) {
